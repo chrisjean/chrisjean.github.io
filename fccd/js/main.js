4cdfcd606a21387f831086687477db304f816bba
Christopher Jean <chrisj@multiprocess.com>
2019-10-25 08:52:58 -0400

    Added FCCD Pricing Page
    Added FCCD Pricing Page
    
    Signed-off-by: Christopher Jean <chrisj@multiprocess.com>

---
+++ fccd/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $("#billingType").change(function() {
+        if (this.checked) {
+            $(".monthlyBilling").addClass("disabled");
+            $(".annualBilling").removeClass("disabled");
+            $(".annualBilling").addClass("enabled");
+            setAnnualPrice();
+        }
+        else
+        {
+            $(".annualBilling").addClass("disabled");
+
+            $(".monthlyBilling").removeClass("disabled");
+
+            $(".monthlyBilling").addClass("enabled");
+
+            setMonthlyPrice();
+        }
+      
+    });
+  });
+
+function setAnnualPrice() {
+    var prices = $(".price") ;
+
+    for(var i = 0; i<prices.length; i++) {
+        var value = prices[i].textContent;
+
+        var outputValue = parseFloat(value.substring(1)) - parseFloat(80);
+
+        prices[i].textContent = '$' + outputValue;
+
+    }
+}
+
+function setMonthlyPrice() {
+    var prices = $(".price") ;
+
+    for(var i = 0; i<prices.length; i++) {
+        var value = prices[i].textContent;
+
+        var outputValue = parseFloat(value.substring(1)) + parseFloat(80);
+
+        prices[i].textContent = '$' + outputValue;
+
+    }
+
+}
---
+++ home/1/README.md
@@ -0,0 +1,2 @@
+# chrisjean
+WebPage
