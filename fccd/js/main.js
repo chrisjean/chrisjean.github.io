c9452b50dcefaf88895b50a4172855adee38437e
Christopher Jean <chrisj@multiprocess.com>
2019-10-29 13:25:48 -0400

    Added Footer Drawer
    Signed-off-by: Christopher Jean <chrisj@multiprocess.com>

--- fccd/js/main.js
+++ fccd/js/main.js
@@ -1,5 +1,49 @@
+$("#annualBilling").off().on('click', setAnnualPrice());
+$("#monthlyBilling").off().on('click', setMonthlyPrice());
+
+var currentPrice = "monthly";
+//setMonthlyPrice();
+
+var sliderState = false;
+
+function closeFooter() {
+    hideDrawerContent();
+    toggleSlider();
+}
+
+function toggleSlider(contentType) {
+
+ 
+    if(sliderState === false) {
+        $('#footerSlideContainer').animate({ height: '50%' });
+        $('#' + contentType).animate({ height: '50%' });
+        //$(this).css('backgroundPosition', 'bottom left');
+        $('#pageOverlay').toggle();
+        sliderState = true;
+    } else {
+        $('#footerSlideContainer').animate({ height: '0px' });
+        $('#' + contentType).animate({ height: '0px' });
+        //$(this).css('backgroundPosition', 'top left');
+        sliderState = false;
+        hideDrawerContent();
+        $('#pageOverlay').toggle();
+    }
+			
+};
+
+
 $(document).ready(function() {
-    $("#billingType").change(function() {
+
+   // $("#monthlyBilling").off().on('click', setMonthlyPrice());
+
+   // $("#annualBilling").off().on('click', setAnnualPrice());
+
+   
+   setMonthlyPrice();
+  
+
+    /*$("#billingType").change(function() {
+        
         if (this.checked) {
             $(".monthlyBilling").addClass("disabled");
             $(".annualBilling").removeClass("disabled");
@@ -16,33 +60,87 @@ $(document).ready(function() {
 
             setMonthlyPrice();
         }
+
+
       
-    });
+    });*/
   });
 
 function setAnnualPrice() {
-    var prices = $(".price") ;
 
-    for(var i = 0; i<prices.length; i++) {
-        var value = prices[i].textContent;
+    if(currentPrice === "monthly")
+    {
+        $("#monthlyBilling").addClass("disabled");
+        $("#annualBilling").removeClass("disabled");
+        $("#annualBilling").addClass("enabled");
+        $("#monthlyBilling").removeClass("enabled");
 
-        var outputValue = parseFloat(value.substring(1)) - parseFloat(80);
 
-        prices[i].textContent = '$' + outputValue;
+        var prices = $(".price") ;
 
+        for(var i = 0; i<prices.length; i++) {
+            var value = prices[i].textContent;
+
+            var outputValue = parseFloat(value.substring(1)) - parseFloat(80);
+
+            prices[i].textContent = '$' + outputValue;
+
+        }
+
+        currentPrice = "annual";
     }
 }
 
 function setMonthlyPrice() {
-    var prices = $(".price") ;
 
-    for(var i = 0; i<prices.length; i++) {
-        var value = prices[i].textContent;
+    if(currentPrice === "annual")
+    {
+        $("#annualBilling").addClass("disabled");
+        $("#monthlyBilling").removeClass("disabled");
+        $("#monthlyBilling").addClass("enabled");
+        $("#annualBilling").removeClass("enabled");
+        
+        var prices = $(".price") ;
 
-        var outputValue = parseFloat(value.substring(1)) + parseFloat(80);
+        for(var i = 0; i<prices.length; i++) {
+            var value = prices[i].textContent;
 
-        prices[i].textContent = '$' + outputValue;
+            var outputValue = parseFloat(value.substring(1)) + parseFloat(80);
 
+            prices[i].textContent = '$' + outputValue;
+
+        }
+
+        currentPrice = "monthly";
     }
 
+    
+}
+
+function hideDrawerContent() {
+
+    $("#POProcessing").addClass("hide");
+    $("#FormsAndLabelPrinting").addClass("hide");
+    $("#InventoryControl").addClass("hide");
+    $("#Quickbooks").addClass("hide");
+    $("#LotControl").addClass("hide");
+    $("#MobileOrderEntry").addClass("hide");
+    $("#MobileFillAndShip").addClass("hide");
+}
+
+
+function drawerSlide(contentType){
+    
+    hideDrawerContent();
+
+    if(contentType === "POProcessing") $("#POProcessing").removeClass("hide");
+    if(contentType === "FormsAndLabelPrinting") $("#FormsAndLabelPrinting").removeClass("hide");
+    if(contentType === "InventoryControl") $("#InventoryControl").removeClass("hide");
+    if(contentType === "Quickbooks") $("#Quickbooks").removeClass("hide");
+    if(contentType === "LotControl") $("#LotControl").removeClass("hide");
+    if(contentType === "MobileOrderEntry") $("#MobileOrderEntry").removeClass("hide");
+    if(contentType === "MobileFillAndShip") $("#MobileFillAndShip").removeClass("hide");
+
+    toggleSlider(contentType);
+    
 }
